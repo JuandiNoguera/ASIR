41d7d0e24ac8ad6ef190af62874928a124035722
Juandi Noguera <juandinogueraide@gmail.com>
2018-02-11 12:15:29 +0100

    He añadido el fichero Ejercicio1

---
+++ Ejercicio1.py
@@ -0,0 +1,5 @@
+from funcionesboletin8 import pasar_segundos, calcular_coste, convertir_euros
+
+tarifa=int(input("Dime el coste por segundo en centimos: "))
+comunicaciones=int(input("Dime el nº de comunicaciones que has realizado: "))
+duracioncomunicaciones=[]
