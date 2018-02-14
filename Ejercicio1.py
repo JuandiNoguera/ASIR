d3564c6a97f3626976520e1d501c09af587e4301
Juandi Noguera <juandinogueraide@gmail.com>
2018-02-14 16:52:23 +0100

    He añadido el fichero Ejercicio1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -3,3 +3,24 @@ from funcionesboletin8 import pasar_segundos, calcular_coste, convertir_euros
 tarifa=int(input("Dime el coste por segundo en centimos: "))
 comunicaciones=int(input("Dime el nº de comunicaciones que has realizado: "))
 duracioncomunicaciones=[]
+segundostotales=0
+llamadas=1
+
+for i in range(comunicaciones):
+	listallamadas=[]
+	comunicacion=input("¿Duracion de la llamada(formato horas/minutos/segundos): ".format(i+1))
+	hora=int(comunicacion.split("/")[0])
+	minuto=int(comunicacion.split("/")[1])
+	segundo=int(comunicacion.split("/")[2])
+	listallamadas.append(llamadas)
+	listallamadas.append(pasar_segundos(hora,minuto,segundo))
+	duracioncomunicaciones.append(listallamadas)
+	llamada+=1
+
+for i in duracioncomunicaciones:
+	segundostotales+=i[1]
+
+for i in duracioncomunicaciones:
+	print("El coste de la llamada {} es {} €".format(i[0],convertir_euros(int(calcular_coste(i[1],tarifa)))))
+
+print("La factura total es de {} €".format(convertir_euros(calcular_coste(segundostotales,tarifa))))
