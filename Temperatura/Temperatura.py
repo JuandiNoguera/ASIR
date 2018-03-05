b59cb24a39f106b3f778f13fd74f62c2c89230e8
Juandi Noguera <juandinogueraide@gmail.com>
2018-03-05 08:27:37 +0100

    He modificado el fichero Temperatura

--- Temperatura/Temperatura.py
+++ Temperatura/Temperatura.py
@@ -1,1 +1,31 @@
+'''Realiza un programa que te pida el nombre de un municipio de la 
+provincia de Sevilla, y si existe te muestre la 
+temperatura máxima y mínima actuales.'''
 
+from lxml import etree
+
+doc=etree.parse("sevilla.xml")
+cps=doc.findall("municipio")
+l1=[]
+l2=[]
+
+for cp in cps:
+	valor=str(cp.attrib["value"])
+	ids=valor.split("-id")
+	l1.append(ids[0])
+	l2.append(ids[1])
+	print("Selecciona un municipio",l1)
+
+municipios=input("Introduce el nombre del municipio: ")
+url=etree.parse("http://www.aemet.es/xml/municipios/localidad_41038.xml")
+
+for x,y in zip(l1,l2):
+	if municipios==x:
+		codigo=y
+		url=etree.parse("http://www.aemet.es/xml/municipios/localidad_"+codigo+".xml")
+
+	maxima=url.find("prediccion/dia/temperatura/maxima")
+	minima=url.find("prediccion/dia/temperatura/minima")
+
+print("Las maxima son",maxima.text)
+print("Las minima son",minima.text)
